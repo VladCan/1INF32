8ee923d356e4575b3d3057757ea4c6c3c6d5f138
VladCan <vladymir.canchari@gmail.com>
2024-11-26 14:32:46 -0500

    Preferencias

--- HorarioMedicosGenetico/main.cpp
+++ HorarioMedicosGenetico/main.cpp
@@ -44,8 +44,27 @@ void generaHorario(vector <int> &vaux, int n) {
 
 //, vector<vector<int>> Vrequisitos
 
-int calcularFitnessHorario(vector<int> cromo) {
+int calcularFitnessHorario(vector<int> cromo,map<int, vector<int>> preferencias) {
     int fitness=0;
+    
+    for (int i = 0; i < cromo.size(); i += 8) {
+        int idMedico = cromo[i]; // ID del médico
+        const auto& horario = vector<int>(cromo.begin() + i + 1, cromo.begin() + i + 8); // Horario del médico
+
+        // Verificar si el médico tiene preferencias definidas
+        if (preferencias.find(idMedico) != preferencias.end()) {
+            const auto& pref = preferencias.at(idMedico);
+
+            // Evaluar el horario comparándolo con las preferencias
+            for (int dia = 0; dia < horario.size(); ++dia) {
+                // Si el turno asignado coincide con la preferencia, sumar fitness
+                if (horario[dia] == pref[dia]) {
+                    fitness += 4; // Coincidencia exacta
+                }
+            }
+        }
+    }
+    
     
     
     //Lunes Mañana Martes noche
@@ -91,14 +110,14 @@ void generapoblacioninicial(vector<vector<int>> &poblacion) {
 
 }
 
-void muestrapoblacion(vector<vector<int>>poblacion) {
+void muestrapoblacion(vector<vector<int>>poblacion,map<int, vector<int>> preferencias) {
 
     for (int i = 0; i < poblacion.size(); i++) {
         for (int j = 0; j < poblacion[i].size(); j++) {;
             if (j % 8 == 0) cout << "Medico";
             cout << poblacion[i][j] << "  ";
         }
-        cout<<"fit: "<<calcularFitnessHorario(poblacion[i]);
+        cout<<"fit: "<<calcularFitnessHorario(poblacion[i],preferencias);
         cout << endl;
     }
 }
@@ -107,12 +126,21 @@ int main(int argc, char** argv) {
 
     int cont = 0;
     vector<vector<int>> poblacion;
-
-
+    map<int, vector<int>> preferencias;
+    // Médico 0: No trabajar lunes, martes, miércoles; jueves mañana, viernes tarde, sábado noche, domingo tarde
+    preferencias[0] = {0, 0, 0, 1, 2, 3, 2};
+    // Médico 1: Prefiere trabajar lunes y martes por la mañana, miércoles tarde, jueves tarde, viernes noche, no sábado ni domingo
+    preferencias[1] = {1, 1, 2, 2, 3, 0, 0};
+    // Médico 2: Prefiere no trabajar lunes, martes noche, miércoles mañana, jueves no trabaja, viernes tarde, sábado noche, domingo mañana
+    preferencias[2] = {0, 3, 1, 0, 2, 3, 1};
+    // Médico 3: Prefiere no trabajar lunes, martes y miércoles; jueves noche, viernes mañana, sábado tarde, domingo noche
+    preferencias[3] = {0, 0, 0, 3, 1, 2, 3};
+    
+    
     generapoblacioninicial(poblacion);
 
 
-    muestrapoblacion(poblacion);
+    muestrapoblacion(poblacion,preferencias);
 
     return 0;
 }
